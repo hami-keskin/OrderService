e6731a538780202cccc7eff5d4e37ffb9f5551d8
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-29 18:18:57 +0300

    Update OrderDto.java

--- src/main/java/com/example/order_service/dto/OrderDto.java
+++ src/main/java/com/example/order_service/dto/OrderDto.java
@@ -3,7 +3,6 @@ package com.example.order_service.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class OrderDto {
@@ -13,5 +12,4 @@ public class OrderDto {
     private Integer customerId;
     private Integer shippingAddressId;
     private Double totalAmount;
-    private List<OrderItemDto> orderItems;
 }
