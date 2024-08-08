a300736df9482fc06fd430bdb1991857a7009fe5
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-08 13:37:03 +0300

    Update LoggerAspect.java

--- src/main/java/com/example/OrderService/aspect/LoggerAspect.java
+++ src/main/java/com/example/OrderService/aspect/LoggerAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,8 +15,18 @@ import org.springframework.stereotype.Component;
 public class LoggerAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
 
-    // Pointcut tanımlaması: @RequestLogger anotasyonu eklenmiş metotları hedef alır
+    // Method level @RequestLogger
     @Pointcut("@annotation(com.example.OrderService.annotation.RequestLogger)")
+    private void methodRequestLogger() {
+    }
+
+    // Class level @RequestLogger
+    @Pointcut("@within(com.example.OrderService.annotation.RequestLogger)")
+    private void classRequestLogger() {
+    }
+
+    // Combined pointcut for both method and class level
+    @Pointcut("methodRequestLogger() || classRequestLogger()")
     private void requestLoggerMethods() {
     }
 
@@ -24,20 +35,20 @@ public class LoggerAspect {
     public Object logRequestResponse(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
 
-        // Log the request details
+        // Yöntem çağrısı öncesi loglama
         logger.info("Executing method: {} with arguments: {}", joinPoint.getSignature(), joinPoint.getArgs());
 
         Object result;
         try {
             result = joinPoint.proceed();
         } catch (Throwable throwable) {
-            // Log the exception
+            // Hata durumunda loglama
             logger.error("Exception in method: {} with cause: {}", joinPoint.getSignature(), throwable.getCause() != null ? throwable.getCause() : "NULL");
             throw throwable;
         }
 
         long timeTaken = System.currentTimeMillis() - startTime;
-        // Log the response details
+        // Yöntem çağrısı sonrası loglama
         logger.info("Method executed: {} returned: {} in: {}ms", joinPoint.getSignature(), result, timeTaken);
 
         return result;
