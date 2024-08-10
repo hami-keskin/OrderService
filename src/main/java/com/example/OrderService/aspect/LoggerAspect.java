fa538d32bddf9709dbb411df0a99f59476dd15b7
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-10 09:49:31 +0300

    +

--- src/main/java/com/example/OrderService/aspect/LoggerAspect.java
+++ src/main/java/com/example/OrderService/aspect/LoggerAspect.java
@@ -1,6 +1,5 @@
 package com.example.OrderService.aspect;
 
-import com.example.OrderService.annotation.RequestLogger;
 import com.example.OrderService.exception.MethodExecutionException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -15,42 +14,33 @@ import org.springframework.stereotype.Component;
 public class LoggerAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
 
-    // Method level @RequestLogger
     @Pointcut("@annotation(com.example.OrderService.annotation.RequestLogger)")
     private void methodRequestLogger() {
     }
 
-    // Class level @RequestLogger
     @Pointcut("@within(com.example.OrderService.annotation.RequestLogger)")
     private void classRequestLogger() {
     }
 
-    // Combined pointcut for both method and class level
     @Pointcut("methodRequestLogger() || classRequestLogger()")
     private void requestLoggerMethods() {
     }
 
-    // Around advice: Hedef metodun öncesinde ve sonrasında çalışır
     @Around("requestLoggerMethods()")
-    public Object logRequestResponse(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object logRequestResponse(ProceedingJoinPoint joinPoint) throws MethodExecutionException {
         long startTime = System.currentTimeMillis();
 
-        // Yöntem çağrısı öncesi loglama
         logger.info("Executing method: {} with arguments: {}", joinPoint.getSignature(), joinPoint.getArgs());
 
         Object result;
         try {
             result = joinPoint.proceed();
         } catch (Throwable throwable) {
-            // Hata durumunda loglama
-            logger.error("Exception in method: {} with cause: {}", joinPoint.getSignature(), throwable.getCause() != null ? throwable.getCause() : "NULL");
-
-            // Özel bir istisna fırlatma
-            throw new MethodExecutionException("Error occurred while executing method: " + joinPoint.getSignature(), throwable);
+            logger.error("Exception occurred in method: {} with cause: {}", joinPoint.getSignature(), throwable.getCause() != null ? throwable.getCause() : "NULL");
+            throw new MethodExecutionException("Error occurred while executing method: " + joinPoint.getSignature() + ". Please check the logs for more details.", throwable);
         }
 
         long timeTaken = System.currentTimeMillis() - startTime;
-        // Yöntem çağrısı sonrası loglama
         logger.info("Method executed: {} returned: {} in: {}ms", joinPoint.getSignature(), result, timeTaken);
 
         return result;
