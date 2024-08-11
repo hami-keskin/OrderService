0b385f484d14de2187704ecf43466ca6527ba16a
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-08-11 11:42:32 +0300

    Update OrderItemController.java

--- src/main/java/com/example/OrderService/controller/OrderItemController.java
+++ src/main/java/com/example/OrderService/controller/OrderItemController.java
@@ -1,5 +1,6 @@
 package com.example.OrderService.controller;
 
+import com.example.OrderService.dto.OrderDto;
 import com.example.OrderService.dto.OrderItemDto;
 import com.example.OrderService.service.OrderItemService;
 import com.example.OrderService.service.OrderService;
@@ -19,27 +20,32 @@ public class OrderItemController {
 
     static final String ORDER_NOT_FOUND_MESSAGE = "Order not found"; // Sabit tanımlandı
 
+    private OrderDto getOrderOrThrow(Integer orderId) {
+        return orderService.getOrderById(orderId)
+                .orElseThrow(() -> new RuntimeException(ORDER_NOT_FOUND_MESSAGE));
+    }
+
     @PostMapping
     public ResponseEntity<OrderItemDto> addOrderItem(@PathVariable Integer orderId, @RequestBody OrderItemDto orderItemDto) {
-        OrderItemDto createdOrderItem = orderItemService.addOrderItem(orderService.getOrderById(orderId).orElseThrow(() -> new RuntimeException(ORDER_NOT_FOUND_MESSAGE)), orderItemDto);
+        OrderItemDto createdOrderItem = orderItemService.addOrderItem(getOrderOrThrow(orderId), orderItemDto);
         return ResponseEntity.ok(createdOrderItem);
     }
 
     @PutMapping("/{orderItemId}")
     public ResponseEntity<OrderItemDto> updateOrderItem(@PathVariable Integer orderId, @PathVariable Integer orderItemId, @RequestBody OrderItemDto orderItemDto) {
-        OrderItemDto updatedOrderItem = orderItemService.updateOrderItem(orderService.getOrderById(orderId).orElseThrow(() -> new RuntimeException(ORDER_NOT_FOUND_MESSAGE)), orderItemId, orderItemDto);
+        OrderItemDto updatedOrderItem = orderItemService.updateOrderItem(getOrderOrThrow(orderId), orderItemId, orderItemDto);
         return ResponseEntity.ok(updatedOrderItem);
     }
 
     @DeleteMapping("/{orderItemId}")
     public ResponseEntity<Void> deleteOrderItem(@PathVariable Integer orderId, @PathVariable Integer orderItemId) {
-        orderItemService.deleteOrderItem(orderService.getOrderById(orderId).orElseThrow(() -> new RuntimeException(ORDER_NOT_FOUND_MESSAGE)), orderItemId);
+        orderItemService.deleteOrderItem(getOrderOrThrow(orderId), orderItemId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping
     public ResponseEntity<List<OrderItemDto>> getOrderItemsByOrderId(@PathVariable Integer orderId) {
-        List<OrderItemDto> orderItems = orderItemService.getOrderItemsByOrderId(orderService.getOrderById(orderId).orElseThrow(() -> new RuntimeException(ORDER_NOT_FOUND_MESSAGE)));
+        List<OrderItemDto> orderItems = orderItemService.getOrderItemsByOrderId(getOrderOrThrow(orderId));
         return ResponseEntity.ok(orderItems);
     }
 }
